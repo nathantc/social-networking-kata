b651bc96e7233a83e2d8d96fff32f74608a539eb
Nathan Carver <nathan@codecarver.io>
2020-05-31 14:00:06 -0400

    Initial commit.

---
+++ src/test/java/PublishingMessage.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PublishingMessage {
+
+
+    @Test
+    public void test() {
+        assertThat("Hello World!").isEqualTo("Hello World!");
+    }
+}
