4c8ebdcec638d54ce8fcd9005a7f0d6ae656b6e1
Nathan Carver <nathan@codecarver.io>
2020-05-31 14:09:37 -0400

    Timeline returns messages.

---
+++ src/main/java/Timeline.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timeline {
+    public List<Message> getMessage() {
+        return new ArrayList<>();
+    }
+}
--- src/test/java/PublishingMessage.java
+++ src/test/java/PublishingMessage.java
@@ -1,12 +1,15 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PublishingMessage {
 
-
     @Test
-    public void test() {
-        assertThat("Hello World!").isEqualTo("Hello World!");
+    public void timelineReturnListOfMessages() {
+        Timeline timeline = new Timeline();
+        List<Message> messages = timeline.getMessage();
+        assertThat(messages.size()).isEqualTo(0);
     }
 }
